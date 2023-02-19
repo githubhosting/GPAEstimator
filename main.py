ff6da4f929b5711894d653292049a31d36f1f622
myselfshravan <shravanrevanna@gmail.com>
2023-02-20 02:42:22 +0530

    added pages and made changes to table

--- main.py
+++ main.py
@@ -7,22 +7,50 @@ from src.exam import micro
 from src.scraper import get_cache, validate_usn
 from src.sis import micro, CACHE_NAME
 
+th_props = [
+	('text-align', 'left'),
+	('font-weight', 'bold'),
+]
+
+td_props = [
+	('text-align', 'center'),
+]
+
+headers_props = [
+	('text-align', 'center'),
+]
+cell_hover = {
+	'selector': 'td-second-child:hover',
+	'props': [('background-color', '#ffffb3')]
+}
+styles = [
+	dict(selector="th", props=th_props),
+	dict(selector="td:nth-child(3)", props=td_props),
+]
+styles_attd = [
+	dict(selector="td:nth-child(3)", props=td_props),
+	dict(selector="td:nth-child(4)", props=td_props),
+]
 st.set_page_config(page_title="Calculla - GPA Calculator", page_icon="📊", layout="centered")
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
-                footer:after {
-                content:'Made with passion by Amith and Shravan'; 
-                visibility: visible;
-	            display: block;
-	            position: relative;
-	            padding: 15px;
-	            top: 2px;
-	            }
+        footer:after {
+        content:'Made with passion by Amith and Shravan'; 
+        visibility: visible;
+        display: block;
+        position: relative;
+        padding: 15px;
+        top: 2px;
+        }
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.title("Calculla - GPA Calculator")
+st.markdown(
+	'Follow the instructions and see the how its calculated <a href="/Instructions-Working" >***Click Here*** </a>',
+	unsafe_allow_html=True)
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["Check CIE Marks", "Grades-Score", "Credit-CGPA", "Crack DOB", "How it works?"])
 
 
@@ -57,7 +85,7 @@ def tab_1():
 			if not m1:
 				st.warning("Invalid USN or DOB", icon="🚨")
 			else:
-				st.subheader(f"Hey {m1['name']}!")
+				st.subheader(f"Hey {m1['name']}! 👋")
 				st.write(f"Here is your CIE Marks for Semester {m1['sem']}")
 				for sub_code, sub in m2.items():
 					marks.append(sub["tot"][0])
@@ -65,20 +93,33 @@ def tab_1():
 					subject_name.append(sub["sub"])
 					attendance.append(sub["attd"])
 				table = pd.DataFrame({"Subject": subject_name, "Marks": marks},
-				                     index=[j for j in range(1, len(marks) + 1)], )
-				st.table(table)
+				                     index=[j for j in range(1, len(marks) + 1)])
+				st.markdown(table.style.set_table_styles(styles).to_html(), unsafe_allow_html=True)
+				total_cie_marks = sum(marks)
+				st.subheader(f"Your Total CIE Marks: {total_cie_marks}/{len(marks) * 50}")
 				short_attenddence = []
-				st.write("Here is your attendance for each subject")
+				st.markdown("""
+				&nbsp;
+				
+				#### Here is your Attendance %
+				""")
 				for j in attendance:
 					if j < 75:
-						short_attenddence.append("🚨")
-					else:
-						short_attenddence.append("✅")
+						short_attenddence.append({subject_name[attendance.index(j)]})
+				attendance = [str(j) + "%" for j in attendance]
+
 				table = pd.DataFrame({"Subject": subject_name,
 				                      "Percentage": attendance,
-				                      "Attendance": short_attenddence},
+				                      # "Attendance": short_attenddence
+				                      },
 				                     index=[k for k in range(1, len(marks) + 1)])
-				st.table(table)
+				st.markdown(table.style.set_table_styles(styles_attd).to_html(), unsafe_allow_html=True)
+				if short_attenddence:
+					st.write("")
+					st.write("You have short attendance in the following subjects")
+					for key in short_attenddence:
+						remove = str(key).replace("{'", "").replace("'}", "")
+						st.warning(remove)
 	elif usn:
 		st.error('Invalid USN', icon="🚨")
 
@@ -110,17 +151,18 @@ def tab_2(year, dept, i, temp, dob):
 			to_score_b = []
 
 			for i in range(len(marks)):
-				const = (90 - marks[i]) * (2 if creds[i] > 2 else 1)
+				lab = subject_name[i].lower().endswith("laboratory")
+				const = (90 - marks[i]) * (2 if not lab else 1)
 				to_score_o.append(const)
-				to_score_ap.append(const - (20 if creds[i] > 2 else 10))
-				to_score_a.append(const - (40 if creds[i] > 2 else 20))
-				to_score_bp.append(const - (60 if creds[i] > 2 else 30))
-				to_score_b.append(const - (80 if creds[i] > 2 else 40))
-				if const > (100 if creds[i] > 2 else 50): to_score_o[-1] = "-"
-				if const > (120 if creds[i] > 2 else 60): to_score_ap[-1] = "-"
-				if const > (140 if creds[i] > 2 else 70): to_score_a[-1] = "-"
-				if const > (160 if creds[i] > 2 else 80): to_score_bp[-1] = "-"
-				if const > (180 if creds[i] > 2 else 90): to_score_b[-1] = "-"
+				to_score_ap.append(const - (20 if not lab else 10))
+				to_score_a.append(const - (40 if not lab else 20))
+				to_score_bp.append(const - (60 if not lab else 30))
+				to_score_b.append(const - (80 if not lab else 40))
+				if const > (100 if not lab else 50): to_score_o[-1] = "-"
+				if const > (120 if not lab else 60): to_score_ap[-1] = "-"
+				if const > (140 if not lab else 70): to_score_a[-1] = "-"
+				if const > (160 if not lab else 80): to_score_bp[-1] = "-"
+				if const > (180 if not lab else 90): to_score_b[-1] = "-"
 
 			table = pd.DataFrame({"Subject": subject_name, "Marks": marks, "O": to_score_o, "A+": to_score_ap,
 			                      "A": to_score_a, "B+": to_score_bp, "B": to_score_b},
@@ -164,7 +206,13 @@ def main():
 	with tab3:
 		tab_3(subject_name_, dob_, creds_)
 	with tab4:
-		st.write("You Dont Have access to this page")
+		st.write(f"{st.experimental_user.email} Dont Have access to this page")
+		if st.experimental_user.email == 'revannamrclk@email.com':
+			st.write("Welcome to the page")
+		elif st.experimental_user.email == 'shravanrevanna158@gmail.com':
+			st.write("Welcome to the page")
+		else:
+			st.write("Please contact us to get access!")
 	# st.title("Crack DOB")
 	# usn = st.text_input("Enter USN number").upper()
 	# if validate_usn(usn):
