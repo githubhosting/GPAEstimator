fcbc9d08c44b4cc10966abad2cf150956052abdd
myselfshravan <shravanrevanna@gmail.com>
2023-02-20 17:00:05 +0530

    .

--- pages/2_Instructions_and_Working.py
+++ pages/2_Instructions_and_Working.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import streamlit as st
 
+
+def local_html(file_name):
+	with open(file_name) as f:
+		st.markdown(f'{f.read()}', unsafe_allow_html=True)
+
+
+local_html("index.html")
+
 th_props = [
 	('text-align', 'left'),
 	('font-weight', 'bold'),
@@ -59,26 +67,31 @@ with tab2:
 	st.write("The grading system is as follows:")
 	creds = [10, 9, 8, 7, 6, 5, 4, 0]
 	df = pd.DataFrame({
-		"Marks Range": ["90 - 100", "80 - 89", "70 - 79", "60 - 69", "50 - 59", "40 - 49", "30 - 39", "0 - 29"],
+		"Marks Range": ["90 - 100", "80 - 89", "70 - 79", "60 - 69", "55 - 59", "50 - 54", "40 - 49", "0 - 39"],
 		"Grade": ["O", "A+", "A", "B+", "B", "C", "P", "F"],
 		"GPA": creds},
 		index=None)
 	st.write(df.style.set_table_styles(styles).to_html(), unsafe_allow_html=True)
+	st.write("")
+	st.subheader("Grade Point Average - GPA")
+	st.write(
+		"Grade Point Average (CGPA), both of which are important performance indices of the student. SGPA is equal to the sum of all the total points earned by the student in a given semester divided by the number of credits registered by the student in that semester. CGPA gives the sum of all the total points earned in all the previous semesters and the current semester divided by the number of credits registered in all these semesters.")
 
 	st.markdown(
 		"""
-		&nbsp;
 		#### Semester Grade point average - SGPA is calculated as follows: \n
+		
+		$$ \\frac{\sum (Course \\ Credits \\times Grade \\ Points) \\ all \\ Courses}{\sum (Course \\ Credits)\\ all \\ Courses} $$ \n
+		
 		""")
+	# st.latex(r'''
+	# 	SGPA = \frac{\sum Course Credits \times Grade Points}{\sum Total Course Credits}
+	# 	''')
+	st.write("#### Cumulative Grade point average - CGPA:", unsafe_allow_html=True)
+	# st.latex(r'''
+	# 	CGPA = \frac{\sum Course Credits \times Grade Points}{\sum Total Credits}
+	# 	''')
 	st.latex(r'''
-		SGPA = \frac{\sum Course Credits \times Grade Points}{\sum Total Course Credits}
+		CGPA = \frac{Sum \ of \ all \ SGPA}{Total \ Credits \ earned}
 		''')
-	st.write("#### Cumulative Grade point average - CGPA", unsafe_allow_html=True)
-	st.latex(r'''
-		CGPA = \frac{\sum Course Credits \times Grade Points}{\sum Total Credits}
-		''')
-	st.caption("Note in CGPA : for all courses, excluding those with “F” and transitional grades until that semester")
-	st.write("#### Credits", unsafe_allow_html=True)
-	st.latex(r'''
-		Credits = \frac{Course Credits}{Course Credits + 1}
-		''')
+	st.write("Note in CGPA : for all courses, excluding those with “F” and transitional grades until that semester")
