bffa593774fb66345b78887ea9078f0352c5568f
myselfshravan <shravanrevanna@gmail.com>
2023-03-16 22:55:57 +0530

    added logs log and added analysis section to admin page

--- pages/5_Admin.py
+++ pages/5_Admin.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import pandas as pd
 import streamlit as st
 from streamlit.components.v1 import html
 
@@ -84,6 +87,51 @@ if check_password():
 		f.seek(0)
 		lines = f.read().replace("\n", "<br/><br/>")
 		html(f"<p style='color: #b5afaf'>{lines}<p/>", height=400, scrolling=True)
+	time_frame = []
+	all_usns = []
+	tokens = []
+
+	with open("data/logs.log", 'r') as f:
+		for _ in range(5): st.write("\n")
+		line = f.readline()
+		for line in f:
+			if "|" in line:
+				line = line.split("|")
+				time_line = line[1]
+				usn = line[2]
+				token = line[5]
+				tokens.append(token)
+				all_usns.append(usn)
+				time_line = time_line.strip()
+				time_ = datetime.strptime(time_line, '%Y-%m-%d %H:%M:%S.%f')
+				time_frame.append(time_)
+		df = pd.DataFrame({"Time": time_frame}, index=range(1, len(time_frame) + 1))
+		dept = []
+		dept_ = []
+		unique_usn = list(set(all_usns))
+		st.write("Total number of USNs: ", len(all_usns))
+		st.write("Total number of unique USNs: ", len(unique_usn))
+		for usn in all_usns:
+			dept.append(usn[4:8])
+		for usn in unique_usn:
+			dept_.append(usn[4:8])
+		unique_dept = list(set(dept))
+		dept_count = []
+		dept_count_ = []
+		for i in unique_dept:
+			dept_count.append(dept.count(i))
+		for j in unique_dept:
+			dept_count_.append(dept_.count(j))
+
+		dept_count, dept_count_, unique_dept = zip(*sorted(zip(dept_count, dept_count_, unique_dept), reverse=True))
+
+		dept_counts = pd.DataFrame({"Dept": unique_dept, "Count Rep": dept_count, "Count Unique": dept_count_})
+		st.subheader("Time wise count")
+		st.line_chart(df, use_container_width=True)
+		st.subheader("Department wise count")
+		st.write(dept_counts)
+		st.subheader("Token wise count")
+		st.write(pd.DataFrame({"Token": tokens}).value_counts())
 
 	for _ in range(5): st.write("\n")
 	st.subheader("Stats")
