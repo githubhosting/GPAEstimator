e6fad115635bab450feeded22c3da9fe7229ec0f
Amith M <amitharun3@gmail.com>
2023-03-25 16:55:39 +0530

    Initial commit

---
+++ src/RIRScraping/__init__.py
@@ -0,0 +1,3 @@
+from .scraper import Scraper, gen_usn, gen_dob, validate_usn, validate_dob, AsyncCache
+from .exam import macro as exam_macro, micro as exam_micro, ExamScraper
+from .sis import macro as sis_macro, micro as sis_micro, SisScraper
