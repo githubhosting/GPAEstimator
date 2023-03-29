244f04efee45f6cc256ee4b6acd2d30572f7bb01
Amith M <amitharun3@gmail.com>
2023-03-29 21:18:24 +0530

    Removing subtree from git history

--- RITScraping2.0/src/RITScraping/__init__.py
+++
@@ -1,3 +0,0 @@
-from .exam import macro as exam_macro, micro as exam_micro, ExamScraper
-from .scraper import Scraper, gen_usn, gen_dob, validate_usn, validate_dob, AsyncCache
-from .sis import macro as sis_macro, micro as sis_micro, SisScraper
