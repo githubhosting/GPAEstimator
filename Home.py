10a1f5f44b528198babc18f29ec24be67b668a9a
Amith M <amitharun3@gmail.com>
2023-03-30 16:10:27 +0530

    creds bug fix

--- Home.py
+++ Home.py
@@ -281,8 +281,7 @@ def tab_2(usn, dob):
             sub_codes, sub_names, sub_attds, sub_marks, sub_max_marks = sub_lists(sis_stats["marks"])
             sgpas = sis_stats["sgpas"]
             all_marks = sis_stats["marks"]
-            sub_creds = sis_stats["creds"]
-            sub_cred_val = list(sub_creds.values())
+            sub_creds = [sis_stats["creds"][k] for k in sub_codes]
             tot_avg = []
             for code, m in all_marks.items():
                 cies, ces = m["cies"], m["ces"]
@@ -292,11 +291,11 @@ def tab_2(usn, dob):
 
             priority = []
             for i in range(len(sub_marks)):
-                score = ((sub_cred_val[i] * (sub_max_marks[i] - sub_marks[i])) + (tot_avg[i] - sub_marks[i]) + (
-                        sub_marks[i] * 0.5) + (sub_cred_val[i] * 2))
+                score = ((sub_creds[i] * (sub_max_marks[i] - sub_marks[i])) + (tot_avg[i] - sub_marks[i]) + (
+                        sub_marks[i] * 0.5) + (sub_creds[i] * 2))
                 priority.append(score)
 
-            zip_list = sorted(zip(sub_marks, sub_cred_val, sub_codes, sub_names, sub_attds, priority),
+            zip_list = sorted(zip(sub_marks, sub_creds, sub_codes, sub_names, sub_attds, priority),
                               key=lambda k: (k[5]), reverse=True)
 
             sub_marks, sub_creds, sub_codes, sub_names, sub_attds, priority = zip(*zip_list)
