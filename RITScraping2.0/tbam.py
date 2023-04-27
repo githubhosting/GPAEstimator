3a457265a479a54acf05d29dc2b1f904aafd60e6
Amith M <amitharun3@gmail.com>
2023-04-27 12:34:06 +0530

    .

---
+++ RITScraping2.0/tbam.py
@@ -0,0 +1,27 @@
+import time
+
+from RITScraping import exam_macro, sis_macro, exam_micro, sis_micro
+
+if __name__ == '__main__':
+    YEAR = 2020
+    DEPT = "IS"
+    TEMP = False
+    ODD = False
+    EVEN = True
+    DRY = False
+
+    t = time.time()
+
+    # macro query
+    # exam_stats = exam_macro(YEAR, DEPT, TEMP, EVEN, start=1, stop=151, dry=DRY)
+    # usns = [int(stat['usn'][7:10]) for stat in exam_stats if stat]
+    # usns = range(1, 146)
+    # sis_stats = sis_macro(YEAR, DEPT, usns, temp=TEMP, odd=ODD, dry=DRY)
+
+    # micro query
+    usn = "1MS20IS136"
+    print(exam_micro(usn, even=EVEN))
+    print(sis_micro(usn, odd=False))
+    print(sis_micro(usn, odd=True))
+
+    print(f"Time taken: {time.time() - t:.2f}s")
