f708ca4d30a911d3ebfa4062384b513dda1973ae
Shravan Revanna <94772842+githubhosting@users.noreply.github.com>
2023-08-26 16:20:25 +0530

    Update sis.py
    added parentseven/ to line 52

--- RITScraping2.0/src/RITScraping/sis.py
+++ RITScraping2.0/src/RITScraping/sis.py
@@ -49,7 +49,7 @@ class SisScraper(DobCracker, Scraper):
 
     def __init__(self, odd=False):
         super(SisScraper, self).__init__()
-        self.URL = self.BASE_URL + ("parentsodd/" if odd else "")
+        self.URL = self.BASE_URL + ("parentsodd/" if odd else "parentseven/")
         self.odd = odd
 
     def __aexit__(self, exc_type, exc_val, exc_tb):
